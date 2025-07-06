5ac393f43df95132d739ed9643b30e2931c7c600
omaldonado26 <oemaldonado@uth.hn>
2025-07-06 08:39:17 -0600

    ultimo

--- app/src/main/java/com/example/deteccion_pose/ContactoActivity.java
+++ app/src/main/java/com/example/deteccion_pose/ContactoActivity.java
@@ -3,7 +3,7 @@ package com.example.deteccion_pose;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
+//solo para ver si hice push bien
 public class ContactoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
